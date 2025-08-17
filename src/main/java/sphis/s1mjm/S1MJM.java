b91c573678bb79b63c0423b88e02d41098db09e0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 12:53:46 -0500

    I used https://fabricmc.net/develop/template rq

--- src/main/java/sphis/s1mjm/S1MJM.java
+++ src/main/java/sphis/s1mjm/S1MJM.java
@@ -1,11 +1,11 @@
-package com.example;
+package sphis.s1mjm;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class S1MJM implements ModInitializer {
 	public static final String MOD_ID = "s1mjm";
 
 	// This logger is used to write text to the console and the log file.
