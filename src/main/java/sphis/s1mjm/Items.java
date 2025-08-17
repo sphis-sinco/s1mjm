bee8c4a2118c42682f29eab34ee4330d50a349d1
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 13:10:26 -0500

    uhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ src/main/java/sphis/s1mjm/Items.java
@@ -0,0 +1,17 @@
+package sphis.s1mjm;
+
+public class Items {
+	public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+		// Create the item key.
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FabricDocsReference.MOD_ID, name));
+
+		// Create the item instance.
+		Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item.
+		Registry.register(Registries.ITEM, itemKey, item);
+
+		return item;
+	}
+
+}
